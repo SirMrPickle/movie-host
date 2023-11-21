d816e59ebf0b43175f59a4fc8cb9233dbd68aa68
SirMrPickle <105758809+SirMrPickle@users.noreply.github.com>
2023-11-20 22:48:33 -0600

    initial commit

---
+++ movie-web/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://worker-curly-band-7d82.easysay666.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTViYzJjNzA1YjNkNmQ3ZDJkZjBhYjhhMWI0NWFlNiIsInN1YiI6IjYzYTkxNzNhMjcxY2E1MDA5MzAwMDI3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IoRvXKvbdzyKgJCsLiW8mAeu9O9JbE7RvFWbSC_zSXw"
+};
